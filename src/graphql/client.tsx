e054e69c0979326c775336084557fb3164084a22
Alex Krantz <alex@krantz.dev>
2024-04-27 23:33:37 -0700

    Add basic graphql error handling

--- src/graphql/client.tsx
+++ src/graphql/client.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ApolloLink, HttpLink, NormalizedCacheObject } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 import { createPersistedQueryLink } from '@apollo/client/link/persisted-queries';
 import { RetryLink } from '@apollo/client/link/retry';
 import {
@@ -30,7 +31,15 @@ function makeClient(): NextSSRApolloClient<NormalizedCacheObject> {
       loadManifest: () => import('./persisted-query-manifest.json'),
     }),
   );
-  const link = ApolloLink.from([persistedQueries, retry, http]);
+  const errorHandler = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      for (const { message, locations, path } of graphQLErrors) {
+        console.log(`[GraphQL error]: ${message} at ${path} in ${locations} query`);
+      }
+    }
+    if (networkError) console.error(`[Network error]: ${networkError}`);
+  });
+  const link = ApolloLink.from([errorHandler, persistedQueries, retry, http]);
 
   return new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(),
