a27ba4c446a33a53ef5189b89e52c4c04593242a
Alex Krantz <alex@krantz.dev>
2024-04-28 02:46:24 -0700

    Export graphql entities from generated code

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,1 +1,12 @@
 export { ApolloClientProvider } from './provider';
+export type {
+  CustomDomain,
+  Event,
+  Identity,
+  Organization,
+  Organizer,
+  Participant,
+  Provider,
+  Role,
+  User,
+} from './types';
