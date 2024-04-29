5514b968eee4bc09a46883533b56dc54a138511c
Alex Krantz <alex@krantz.dev>
2024-04-28 19:17:03 -0700

    Make providing defaults required

--- src/components/form/hook.ts
+++ src/components/form/hook.ts
@@ -3,27 +3,22 @@ import { useForm as useFormOriginal } from 'react-hook-form';
 import type { DefaultValues, UseFormProps, UseFormReturn } from 'react-hook-form';
 import { z } from 'zod';
 
-type Props<TSchema extends z.AnyZodObject> = Omit<UseFormProps<z.infer<TSchema>>, 'mode' | 'resolver'> & {
+type Props<TSchema extends z.AnyZodObject> = Omit<
+  UseFormProps<z.infer<TSchema>>,
+  'mode' | 'resolver' | 'defaultValues'
+> & {
   schema: TSchema;
+  defaults: DefaultValues<z.infer<TSchema>>;
 };
-
-const getDefaultsForSchema = <TSchema extends z.AnyZodObject>(schema: TSchema): DefaultValues<z.infer<TSchema>> =>
-  Object.fromEntries(
-    Object.entries(schema.shape).map(([key, value]) => {
-      if (value instanceof z.ZodDefault) return [key, value._def.defaultValue()];
-      if (value instanceof z.ZodObject) return [key, getDefaultsForSchema(value)];
-      return [key, undefined];
-    }),
-  ) as DefaultValues<z.infer<TSchema>>;
 
 export const useForm = <TSchema extends z.AnyZodObject>({
   schema,
-  defaultValues,
+  defaults,
   ...props
 }: Props<TSchema>): UseFormReturn<z.infer<TSchema>> =>
   useFormOriginal<z.infer<TSchema>>({
     mode: 'all',
     resolver: schema !== undefined ? zodResolver(schema) : undefined,
-    defaultValues: defaultValues === undefined ? getDefaultsForSchema(schema) : defaultValues,
+    defaultValues: defaults,
     ...props,
   });
