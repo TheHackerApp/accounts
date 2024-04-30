61124e7f4624843b6fe1b0c50508a512eee26fe2
Alex Krantz <alex@krantz.dev>
2024-04-30 00:28:07 -0700

    Change language used for sign in page

--- src/app/(authenticating)/login/page.tsx
+++ src/app/(authenticating)/login/page.tsx
@@ -8,15 +8,15 @@ export default function Login(): ReactNode {
   return (
     <>
       <h1 className="pb-4 text-left text-3xl font-semibold">
-        Log In
-        <span aria-label="emoji" className="ml-2" role="img">
+        <span aria-label="emoji" className="mr-2" role="img">
           👋
         </span>
+        Sign in
       </h1>
+
       <Suspense fallback={<Spinner />}>
         <ProviderList />
       </Suspense>
-      <p className="text-center text-xs">Don&apos;t have an account?</p>
     </>
   );
 }
