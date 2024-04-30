aea868c58de44d734c9585f28f22e51831153f95
Alex Krantz <alex@krantz.dev>
2024-04-29 23:45:10 -0700

    Allow providing initial errors on the form

--- src/components/form/hook.ts
+++ src/components/form/hook.ts
@@ -1,24 +1,37 @@
-import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm as useFormOriginal } from 'react-hook-form';
-import type { DefaultValues, UseFormProps, UseFormReturn } from 'react-hook-form';
-import { z } from 'zod';
+import type { DefaultValues, FieldErrors, UseFormProps, UseFormReturn } from 'react-hook-form';
+import { ZodError, z } from 'zod';
+
+import { fromZodError, resolver } from './resolver';
 
 type Props<TSchema extends z.AnyZodObject> = Omit<
   UseFormProps<z.infer<TSchema>>,
-  'mode' | 'resolver' | 'defaultValues'
+  'mode' | 'resolver' | 'criteriaMode' | 'defaultValues' | 'errors'
 > & {
   schema: TSchema;
   defaults: DefaultValues<z.infer<TSchema>>;
+  errors?: ZodError<z.infer<TSchema>> | FieldErrors<z.infer<TSchema>>;
 };
 
 export const useForm = <TSchema extends z.AnyZodObject>({
   schema,
   defaults,
+  errors,
   ...props
 }: Props<TSchema>): UseFormReturn<z.infer<TSchema>> =>
   useFormOriginal<z.infer<TSchema>>({
     mode: 'all',
-    resolver: schema !== undefined ? zodResolver(schema) : undefined,
+    resolver: schema !== undefined ? resolver(schema) : undefined,
+    criteriaMode: 'all',
     defaultValues: defaults,
+    errors: transformErrors(errors),
     ...props,
   });
+
+const transformErrors = <TSchema extends z.AnyZodObject>(
+  errors?: ZodError<z.infer<TSchema>> | FieldErrors<z.infer<TSchema>>,
+): FieldErrors<z.infer<TSchema>> | undefined => {
+  if (errors === undefined) return undefined;
+  else if (errors instanceof ZodError) return fromZodError(errors);
+  else return errors;
+};
