edb9ebee1e4611b0adc5470de281607e010f9c3a
Alex Krantz <alex@krantz.dev>
2024-05-01 00:27:17 -0700

    Allow retrieving current user information on server

---
+++ src/lib/user/index.ts
@@ -0,0 +1,14 @@
+import { User } from '@/graphql';
+import { getClient } from '@/graphql/clients/server';
+
+import { ProfileDocument } from './Profile.graphql';
+
+export type Profile = Omit<User, '__typename' | 'events' | 'identities' | 'organizations'>;
+
+/**
+ * Get the current user's profile details
+ */
+export async function getCurrentUser(): Promise<Profile> {
+  const { data } = await getClient().query({ query: ProfileDocument });
+  return data.me;
+}
