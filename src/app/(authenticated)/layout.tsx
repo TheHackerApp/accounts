ad2a452f4df4464646f970bb584bd997c0e0690f
Alex Krantz <alex@krantz.dev>
2024-05-02 23:36:48 -0700

    Reduce authenticated card size

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren, ReactNode } from 'react';
 export default function Layout({ children }: PropsWithChildren): ReactNode {
   return (
     <div className="flex h-screen justify-center items-center">
-      <div className="m-auto w-full max-w-7xl sm:p-6 lg:p-8">
+      <div className="m-auto w-full max-w-5xl sm:p-6 lg:p-8">
         <div className="divide-y divide-divider overflow-hidden bg-content1 shadow sm:rounded-large">{children}</div>
       </div>
     </div>
