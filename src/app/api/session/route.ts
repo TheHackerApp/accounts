e790561ec08ffa0890c35cf465702efa8b8db155
Alex Krantz <alex@krantz.dev>
2024-06-03 07:18:13 -0700

    Automatically add user to event on session transfer

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,6 +1,8 @@
 import { cookies as requestCookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
+import { AddUserToEventDocument } from '@/app/api/session/AddUserToEvent.graphql';
+import { getClient } from '@/graphql/clients/server';
 import { isContextSuccess, loadContext } from '@/lib/context';
 import { encrypt, loadKey, verify } from '@/lib/crypto';
 import { internalServerError } from '@/lib/errors';
@@ -32,8 +34,12 @@ export async function GET(request: Request): Promise<Response> {
   }
   if (context.scope.kind !== 'event') return Response.json({ error: 'invalid event' }, { status: 400 });
 
-  // TODO: handle users being organizers for an event
-  // TODO: handle users not being part of an event
+  if (context.user.role === null) {
+    const wasSuccessful = await addUserToEvent(context.scope.event, context.user.id);
+    if (!wasSuccessful) return internalServerError(NextResponse);
+  } else if (context.user.role !== 'participant') {
+    // TODO: handle users being organizers for an event
+  }
 
   const payload = await generatePayload(session!.value, context.scope.event);
   return redirect(`${SCHEME}://${query.domain}/api/authenticate/${payload}`);
@@ -55,6 +61,29 @@ async function isRequestValid(query: Record<string, string>): Promise<boolean> {
   const timestamp = parseInt(query.timestamp, 10);
   if (now - timestamp > MAX_SKEW) return false;
 
+  return true;
+}
+
+async function addUserToEvent(event: string, userId: number): Promise<boolean> {
+  const client = getClient();
+  const { data, errors } = await client.mutate({
+    mutation: AddUserToEventDocument,
+    variables: { input: { event, userId } },
+  });
+  if (!data || errors) {
+    console.error(`[session] failed to add user to event (user=${userId} event=${event}): graphql error`);
+    return false;
+  } else if (data.addUserToEvent.userErrors.length > 0) {
+    const errors = data.addUserToEvent.userErrors
+      .map((error) => `${error.message} (${error.field.join('.')})`)
+      .join(', ');
+    console.error(`[session] failed to add user to event (user=${userId} event=${event}): ${errors}`);
+    return false;
+  }
+
+  console.log(
+    `[session] added user to event: user=${data.addUserToEvent.user?.id} event=${data.addUserToEvent.event?.slug}`,
+  );
   return true;
 }
 
